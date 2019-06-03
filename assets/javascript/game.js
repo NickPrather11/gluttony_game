13b34c5bf138b0e82fb541e512fdd0422a65767f
Flying Serpent <nickprather11@gmail.com>
2019-06-03 13:23:21 -0400

    script tags moved to body, game.js mostly working, still some bugs in adding ranValArray nums to images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,60 @@
 //create variables for random values (each button, goal number), score variable, Game Over boolean variable
 var ranValArray = [];
-var goalNum = 0;
+var imgArray = ["assets/images/whiskey.png", "assets/images/beer.jpg", "assets/images/cigarette.jpg", "assets/images/pizza.png", "assets/images/chocolate_bar.png"];
+var goalNum = Math.floor((Math.random() * 101) + 19);
 var score = 0;
-var gameOver = false;
+var win = 0;
+var lose = 0;
 
-//create reset function with new random values, score variable at 0, gameOver=false
+
+console.log("goalNum " + goalNum);
+
+//create reset function with new random values, score variable at 0, update wins/losses
 reset = function(){
     goalNum = Math.floor((Math.random() * 101) + 19);
     score = 0;
-    gameOver = false;
-}
+    $("#winLoseCount").text("<div>" + "Wins: " + win + "</div>" + "<div>" + "Losses: " + lose + "</div>");
+};
 
 //assign random values to each button
 ranVals = function(){
+    for(i = 0; i < 5; i++){
+        ranValArray.push(Math.ceil(Math.random() * 12));
+    }
+};
 
+ranVals();
+console.log("ranValArray " + ranValArray);
+
+for(i = 0; i < imgArray.length; i++){
+    var item = $("<img>");
+    item.attr("src", imgArray[i]);
+    item.attr("class", "itemButton");
+    item.attr("data-value", ranValArray[i]);
+    $("#itemButtonsDiv").append(item);
 }
 
+
+
+// Display goalNum and win/lose count
+$("#goalNum").text(goalNum);
+$("#winLoseCount").text("<div>" + "Wins: " + win + "</div>" + "<div>" + "Losses: " + lose + "</div>");
+
 //click event adding value of each button to score variable
+$(".itemButton").on("click", function(){
+    score += this.value;
+    $("#score").text(score);
+    console.log(score);
+})
 
-//if score variable equals goal number then Win (gameOver=true), else if score variable > goal number Lose (gameOver=true)
+//if score variable equals goal number then Win, else if score variable > goal number Lose
+if(score === goalNum){
+    alert("You Win!");
+    win++;
+    reset();
+} else if(score > goalNum){
+    alert("You Got Sick!")
+    lose++;
+    reset();
+}
 
-// if gameOver=true, reset
